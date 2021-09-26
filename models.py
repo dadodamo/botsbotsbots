2e3a0b7e0c44a21f9657e76f7d7f17ad8fa902a2
Serkan <serkan.2009@hotmail.de>
2021-09-27 00:34:02 +0200

    Test for secure login

---
+++ models.py
@@ -0,0 +1,6 @@
+from . import db
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True) # primary keys are required by SQLAlchemy
+    password = db.Column(db.String(100))
+    login = db.Column(db.String(1000))
